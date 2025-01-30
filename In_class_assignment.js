42ba3586875586cab720c5649757e9096395ff78
danielaj2004 <danielajoya12@gmail.com>
2025-01-30 16:37:43 -0500

    Completed in class assignment

--- In_class_assignment.js
+++ In_class_assignment.js
@@ -20,3 +20,22 @@ if (score >= 60) {
 } else {
     console.log("failed");
 }
+
+// Task 3
+
+for (let i = 10; i <= 1; i--) {
+console.log(i);
+}
+
+// task 4
+let num = 2;
+while (num < 10) {
+    console.log(num);
+    num += 2;
+}
+
+//task 5
+function calculatesquare(num) {
+    return num * num;
+}
+console.log(calculateSquare(4)); // outputs: 16
